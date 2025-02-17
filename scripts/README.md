5af0a5d727b328c6a8ea5b97893642e8b517e1c4
Saladino93 <saladino_93@hotmail.it>
2025-02-17 20:44:11 +0100

    cleaning scripts

---
+++ scripts/README.md
@@ -0,0 +1,11 @@
+## Running code
+
+`python param_joint.py -c spt3g.yaml`
+
+There is also QE bias-hardening in a different parameter file
+
+`python param_joint_bh.py -c spt3g.yaml`
+
+## Making plots
+
+`python plot_noise.py --config spt3g`
--- scripts/plotting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure the script stops on errors
-set -e
-
-
-
-python plot_xcorr.py --configs config_alpha_alpha config_full_alpha --subset_selected a --itrs 0 1 5 --outname xcorr_alpha_alpha.pdf
-python plot_xcorr.py --configs config_full_alpha_lensing config_full_alpha_disabled_lensing --subset_selected a p o --itrs 0 1 6 15 --outname xcorr_alpha_lensing.pdf
